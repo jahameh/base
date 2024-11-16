2b0b1f45dc00f10fe9c3566ce0446e43556d0949
jahameh <swervecreations@outlook.com>
2024-11-15 19:57:07 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "could not register service worker";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
